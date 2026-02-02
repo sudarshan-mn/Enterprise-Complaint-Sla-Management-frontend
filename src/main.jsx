ad0a13a16be0d907da06a404ff4c150dd169d547
sudarshan-mn <sudarshan.mn2004@gmail.com>
2026-02-02 21:03:23 +0530

    404

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 import "./index.css";
@@ -9,8 +9,8 @@ import "./styles/theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/Enterprise-Complaint-Sla-Management-frontend/">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Enterprise-Complaint-Sla-Management-frontend/",
+  server: {
+    historyApiFallback: true,
+  },
 });
