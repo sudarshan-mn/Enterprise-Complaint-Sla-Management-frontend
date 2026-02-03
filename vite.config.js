8b9d4da37e2cd02b3cffd4acb36706a9b021153d
sudarshan-mn <sudarshan.mn2004@gmail.com>
2026-02-03 12:21:11 +0530

    fixed dist

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          recharts: ["recharts"],
+          vendor: ["react", "react-dom", "react-router-dom"],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
 });
